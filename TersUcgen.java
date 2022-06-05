705e1427258d41e9d7684458d02b4b9b7536550f
Ece Coşkunçay <62241891+ececsk@users.noreply.github.com>
2022-06-05 21:08:12 +0300

    Update TersUcgen.java

--- TersUcgen.java
+++ TersUcgen.java
@@ -2,8 +2,20 @@
 public class TersUcgen {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        for (int i=1; i<=10; i++)
+        {
+            for (int k=1; k<i; k++)
+            {
+                System.out.print(" ");
+            }
+            System.out.print("*");
 
-	}
+            for (int j=1; j<=(10*2)-(i*2); j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 
 }
