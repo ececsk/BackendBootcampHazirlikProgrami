0bb6c363a3397bd0063d81e72bbe6ca1d545c1b7
Ece Coşkunçay <62241891+ececsk@users.noreply.github.com>
2022-06-05 21:04:23 +0300

    Update EbobEkok.java

--- EbobEkok.java
+++ EbobEkok.java
@@ -1,9 +1,35 @@
-
+import java.util.Scanner;
 public class EbobEkok {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//iki sayının ebob ve ekoklarını bulan program 
+		  int n1, n2 ,a ,ebob = 1, i =1 ,k = 1;
+	        Scanner input = new Scanner(System.in);
 
-	}
+	        System.out.print("1.Sayı : ");
+	         n1 = input.nextInt();
 
-}
+	        System.out.print("2.Sayı: ");
+	         n2 = input.nextInt();
+
+	        a = Math.min(n1, n2);
+
+	        while (i <= a) {
+	            i++;
+	            if (n1 % i == 0 && n2 % i == 0) {
+	                ebob = i;
+	            }
+	        }
+	        System.out.println("Girdiginiz iki sayının EBOB'u: " + ebob);
+
+	        while (k <= n1 * n2) {
+	            k++;
+	            if (k % n1 == 0 && k % n2 == 0) {
+	                System.out.println("Girdiginiz iki sayının EKOK'u: " + k);
+	                break;
+	            }
+	        }
+
+	        
+
+	    }
